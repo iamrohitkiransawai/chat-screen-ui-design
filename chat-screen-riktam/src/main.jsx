c6f8e4b777cd76376b8751438e6aa04256dd5746
iamrohitkiransawai <rohitsawai77@gmail.com>
2023-02-11 20:28:43 +0530

    Slice created and store added

--- chat-screen-riktam/src/main.jsx
+++ chat-screen-riktam/src/main.jsx
@@ -1,11 +1,16 @@
-// import { CssBaseline } from "@mui/material";
-import { CssBaseline } from "@mui/material";
+import { configureStore } from "@reduxjs/toolkit";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import rootReducer from "../src/slices/index";
+import { Provider } from "react-redux";
 import App from "./App";
+
+const store = configureStore({ reducer: rootReducer });
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
