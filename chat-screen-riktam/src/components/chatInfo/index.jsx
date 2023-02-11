8dea7144340d5ec4f87069f6cd6aafbddd3a3361
iamrohitkiransawai <rohitsawai77@gmail.com>
2023-02-12 02:31:20 +0530

    Right side middle graph comp done

--- chat-screen-riktam/src/components/chatInfo/index.jsx
+++ chat-screen-riktam/src/components/chatInfo/index.jsx
@@ -1,10 +1,76 @@
-import { Grid, Paper } from "@mui/material";
-import React from "react";
+import {
+  Avatar,
+  Grid,
+  List,
+  ListItemAvatar,
+  ListItemText,
+  Paper,
+} from "@mui/material";
+import React, { useState } from "react";
+import {
+  ChatContainer,
+  MessageItem,
+  MessageList,
+  SendButton,
+  MessageInput,
+  MessageInputContainer,
+} from "./styles";
+import SendIcon from "@mui/icons-material/Send";
+import { useDispatch, useSelector } from "react-redux";
+import { messageSelector } from "../../slices";
 
 const ChatInfo = () => {
+  const [message, setMessage] = useState("");
+  const messages = useSelector(messageSelector);
+  const dispatch = useDispatch();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(
+      messagesSlice.actions.addMessage({
+        text: message,
+        isSentByUser: true,
+      })
+    );
+    setMessage("");
+  };
   return (
     <Grid item xs={12} sm={6} md={6} lg={6}>
-      <Paper>ChatInfo</Paper>
+      <ChatContainer>
+        <MessageList>
+          <List>
+            {messages &&
+              messages.map((message, index) => (
+                <MessageItem
+                  key={index}
+                  align={message.isSentByUser ? "right" : "left"}
+                >
+                  {!message.isSentByUser && (
+                    <ListItemAvatar>
+                      <Avatar>O</Avatar>
+                    </ListItemAvatar>
+                  )}
+                  <ListItemText primary={message.text} />
+                  {message.isSentByUser && (
+                    <ListItemAvatar>
+                      <Avatar>U</Avatar>
+                    </ListItemAvatar>
+                  )}
+                </MessageItem>
+              ))}
+          </List>
+        </MessageList>
+      </ChatContainer>
+      <MessageInputContainer>
+        <MessageInput
+          label="Type a message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+        <SendButton type="submit" onClick={handleSubmit}>
+          <SendIcon />
+        </SendButton>
+      </MessageInputContainer>
     </Grid>
   );
 };
